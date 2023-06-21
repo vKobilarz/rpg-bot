79d5e0fbad1b225b8c8288c2f3a54b4da275d4e8
Vinicius Kobilarz <vkobilarz@gmail.com>
2023-06-21 19:52:16 -0300

    chore(spring): intializing project

---
+++ src/test/java/com/vkobilarz/rpgbot/RpgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkobilarz.rpgbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
